f2921ce98b7c92fb42d5f60c00edcb6da26cd4b6
Bikram Bishwokarma <bkbikram727@gmail.com>
2025-06-01 17:04:31 +0545

    Using Repositories

--- src/main/java/com/example/spring_boot_demo/SpringBootDemoApplication.java
+++ src/main/java/com/example/spring_boot_demo/SpringBootDemoApplication.java
@@ -4,15 +4,17 @@ import com.example.spring_boot_demo.entities.Address;
 import com.example.spring_boot_demo.entities.Profile;
 import com.example.spring_boot_demo.entities.Tags;
 import com.example.spring_boot_demo.entities.User;
+import com.example.spring_boot_demo.repositories.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDemoApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SpringBootDemoApplication.class, args);
 //		User user = new User();
 //		user.setName("John");
 //		user.setEmail("john@gmail.com");
@@ -25,6 +27,19 @@ public class SpringBootDemoApplication {
 				.password("password")
 				.build();
 
+		var repository = context.getBean(UserRepository.class);
+		//Saving user
+//		repository.save(user);
+
+		//Searching User
+//		repository.findAll().forEach(User -> System.out.println(user.getName()));
+//		var user1 = repository.findById(1L).orElseThrow();
+//		System.out.println(user1.getId()+user1.getEmail());
+
+		//Deleting User
+		repository.deleteById(1L);
+		repository.deleteAll();
+
 //		Address address = Address.builder()
 //				.id(1L)
 //				.street("Jana Jyoti street")
@@ -36,15 +51,16 @@ public class SpringBootDemoApplication {
 //		user.addAddress(address);
 //		var tag = new Tags("tag1");
 //		user.addTag(tag);
-		var profile = Profile.builder()
-						.bio("Bio")
-								.phone_number("555-555-5555")
-				.build();
+//		var profile = Profile.builder()
+//						.bio("Bio")
+//								.phone_number("555-555-5555")
+//				.build();
+//
+//		user.setProfile(profile);
+//		profile.setUser(user);
+//
+//		System.out.println(user);
 
-		user.setProfile(profile);
-		profile.setUser(user);
-		
-		System.out.println(user);
 
 
 
--- src/main/java/com/example/spring_boot_demo/repositories/AddressRepository.java
+++ src/main/java/com/example/spring_boot_demo/repositories/AddressRepository.java
@@ -4,5 +4,5 @@ import com.example.spring_boot_demo.entities.Address;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AddressRepository extends CrudRepository<Address, Long> {
-    
+
 }
